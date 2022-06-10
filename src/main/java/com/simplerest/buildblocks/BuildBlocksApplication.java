7d5122cdd6e81cd63c69558683bb21bd9006b2be
Bohdan Stelmakh <bohdan.stelmakh@softiero.com>
2022-06-10 13:43:56 +0300

    Initial commit

---
+++ src/main/java/com/simplerest/buildblocks/BuildBlocksApplication.java
@@ -0,0 +1,13 @@
+package com.simplerest.buildblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildBlocksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildBlocksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simplerest/buildblocks/BuildBlocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplerest.buildblocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildBlocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
